3447dcb08784b672d8106b1128f68ec8e56ba114
PheY29 <131706411+PheY29@users.noreply.github.com>
2025-02-26 19:36:14 +0100

    Delete test directory

--- test/charge an image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-image_path = "../image to detect/mario.png"
-image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-
-cv2.imshow("Screenshot", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
